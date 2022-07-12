141f7a8d924ca306dfc9f5aba3d7be389d368e33
Luis Adorno <luis.adorno5@upr.edu>
2022-07-12 15:39:42 -0400

    Initial Commit.

---
+++ src/test/java/com/luisyass/helpdesk/HelpDeskServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisyass.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
