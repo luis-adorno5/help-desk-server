73eca4b4ad4e4e406f9a683fbbf9d5034f665b9e
Luis Adorno <luis.adorno5@upr.edu>
2022-07-13 09:15:47 -0400

    Practicing post requests.

--- src/main/java/com/luisyass/helpdesk/domain/account/controller/Controller.java
+++ src/main/java/com/luisyass/helpdesk/domain/account/controller/Controller.java
@@ -3,7 +3,11 @@ package com.luisyass.helpdesk.domain.account.controller;
 import com.luisyass.helpdesk.domain.account.model.Account;
 import com.luisyass.helpdesk.domain.account.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +22,11 @@ public class Controller {
     @GetMapping("/accounts")
     public Iterable<Account> getAllAccounts() {
         return accountService.getAllAccounts();
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/accounts")
+    public void createAccount(@RequestBody Account account) {
+        accountService.create(account);
     }
 }
